7f3ed19ad29951ae59a0f4937685cb88faaa4bf6
babar <babar7495@gmail.com>
2018-03-17 10:43:36 -0700

    toplogo1,2,3 index.php

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sports Week</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+    <section class="container-fluid">
+      <article class=" font-weight-bold">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark ">
+          <div class="container">
+          <ul class="navbar-nav ">
+            <li class="nav-item dropdown ">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Games</a>
+            <div class="dropdown-menu ">
+              <a class="dropdown-item" href="#">Cricket</a>
+              <a class="dropdown-item" href="#">Football</a>
+              <a class="dropdown-item" href="#">Table Tenis</a>
+              <a class="dropdown-item" href="#">Lodo</a>
+              <a class="dropdown-item" href="#">Badminton</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> E-Games</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Call Of Duty</a>
+            <a class="dropdown-item" href="#">Dota 2</a>
+            <a class="dropdown-item" href="#">IGI</a>
+            <a class="dropdown-item" href="#">Need For Speed</a>
+            <a class="dropdown-item" href="#">Other</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Songs</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">National Songs</a>
+          <a class="dropdown-item" href="#">Movies Songs</a>
+          <a class="dropdown-item" href="#">Qawali</a>
+          <a class="dropdown-item" href="#">other</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Stage performance</a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Darama</a>
+        <a class="dropdown-item" href="#">Mimicry</a>
+        <a class="dropdown-item" href="#">Funny performance</a>
+        <a class="dropdown-item" href="#">other</a>
+      </div>
+    </li>
+        </ul>
+        </div>
+        </nav>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner  bg-secondary border border-danger ">
+    <div class="carousel-item active container ">
+      <img class="rounded mx-auto d-block" src="image/toplogo2.jpg" alt="Los Angeles" width= "70%" height= "350">
+    </div>
+    <div class="carousel-item container">
+      <img class="rounded mx-auto d-block" src="image/toplogo.jpg" alt="Chicago" width= "70%" height= "350">
+    </div>
+    <div class="carousel-item container">
+      <img class="rounded mx-auto d-block" src="image/toplogo1.jpg" alt="New York" width= "70%" height= "350">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+        </div>
+        </div>
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark container-fluid">
+        <div class="container">
+            <ul class="navbar-nav ">
+            <li class="nav-item dropdown">
+            <a class="nav-link " href="http://localhost/sports%20week/#"> Home</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link " href="#"> Contact Us</a>
+      </li>
+      </ul>
+
+</div>
+        </nav>
+      </article>
+      <div class="jumbotron jumbotron-fluid">
+      <article class="container ">
+        <div class="row text-secondary col-expand-sm ">
+          <div class="col-3 border border-warning">
+            <h1 class="border-bottom border-info">Events</h1>
+            <a href="#">seminar games registration</a>
+            <p>12 feb 2018</p>
+            <a href="#">Sports week details eg (date to date)</a>
+            <p>12 feb 2018</p>
+            <a href="#">Games includes in sports week every game is very intersting and enjoyable</a>
+            <p>12 feb 2018</p>
+
+          </div>
+          <div class="col-1">    </div>
+          <div class="col-3 border border-warning">
+            <h1 class="border-bottom border-info">Latest News</h1>
+            <a href="#">seminar games registration</a>
+            <p>12 feb 2018</p>
+            <a href="#">Sports week details eg (date to date)</a>
+            <p>12 feb 2018</p>
+            <a href="#">Games includes in sports week every game is very intersting and enjoyable</a>
+            <p>12 feb 2018</p>
+          </div>
+          <div class="col-1">  </div>
+          <div class="col-3 border border-warning">
+            <h1 class="border-bottom border-info">Map</h1>
+            <a href="#">seminar games registration</a>
+            <p>12 feb 2018</p>
+            <a href="#">Sports week details eg (date to date)</a>
+            <p>12 feb 2018</p>
+            <a href="#">Games includes in sports week every game is very intersting and enjoyable</a>
+            <p>12 feb 2018</p>
+
+          </div>
+        </div>
+          </div>
+
+      </article>
+    </section>
+
+  </body>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
